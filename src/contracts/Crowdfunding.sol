5930b211639bc6b3146d393afc50273dd99c9110
rakimsth <raktim@rumsan.com>
2021-05-26 17:26:28 +0545

    dispatchFunds & balanceOfEntireContract Added

--- src/contracts/Crowdfunding.sol
+++ src/contracts/Crowdfunding.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.8.1;
 /* 
 TODO
     - Create a Project : createProject
-    - Fetch all Projects : 
+    - Fetch all Projects : projects
     - Read a Project : getProjectById
-    - Fund an existing Project
-    - Close the Project manually or if endDate expires
-
+    - Fund an existing Project : fundProject
+    - Close the Project manually by owner or if endDate expires : closeProject
+    - Get total Contract Balance : balanceOfContract
+    - Dispatch the ethers to project Owner : dispatchFunds
 */
 contract Crowdfunding {
-    address contractOwner;
     string public name;
     uint public projectCount = 0;
-    mapping(uint => Project) projects;
+    mapping(uint => Project) public projects;
+    mapping(uint => mapping(address => uint)) contributors;
 
     struct Project {
         uint id;
@@ -37,9 +38,21 @@ contract Crowdfunding {
         uint target
     );
 
+    event ProjectFunded(
+        uint id,
+        string name,
+        string desc,
+        address owner,
+        uint256 endDate,
+        bool exists,
+        uint balance,
+        uint target
+    );
+
+    event ProjectEnded(uint id, string name, uint balance);
+
     constructor() {
         name = "Marketplace for crowd funding";
-        contractOwner = msg.sender;
     }
 
     function createProject(string memory _name, string memory _desc, uint256 _endDate, uint _target) public {
@@ -56,6 +69,66 @@ contract Crowdfunding {
         projects[projectCount] = Project(projectCount, _name, _desc, msg.sender, _endDate, true, 0, _target);
         // trigger an event
         emit ProjectCreated(projectCount, _name, _desc, msg.sender, _endDate, true, 0, _target);
+    }  
+
+    function getProjectById(uint _id) public view returns(string memory _name, string memory _desc, uint _target, uint _endDate, address _owner, uint _balance) {
+        //TODO Check if project Exists
+        return (projects[_id].name, projects[_id].desc, projects[_id].target, projects[_id].endDate, projects[_id].owner, projects[_id].balance);
     }
 
+    function fundProject(uint _id) public payable{
+        // Fetch the Project
+        Project memory _project = projects[_id];
+        //Fetch the Project Owner
+        address _owner = _project.owner;
+        //Check if the Project end Date is greater than now
+        require(block.timestamp < _project.endDate, "Project is closed.");
+        // Check if the owner is trying to fund and reject it
+        require(_owner != msg.sender, "Owner can't fund the project created by themselves.");
+        // Make Sure the Project is active
+        require(_project.exists == true, "Project doesn't exists..");
+        // Fund it
+        _project.balance += msg.value;
+        // Update the Project
+        projects[_id] = _project;
+        //hold the money to the contract until it expires or closes;
+
+        // contributors can again send the money
+        contributors[_id][msg.sender] += msg.value;
+        // Trigger the event
+        emit ProjectFunded(projectCount, _project.name, _project.desc, msg.sender, _project.endDate, true, _project.balance, _project.target);
+    }
+
+    function closeProject(uint _id) public {
+        // Fetch the Project
+        Project memory _project = projects[_id];
+        //Fetch the Project Owner
+        address _owner = _project.owner;
+        // Check if the owner is trying to close the project
+        require(_owner == msg.sender, "Only Owner can close the project.");
+        // Check if the project is open or not
+        require(_project.exists == true, "Project is in open state.");
+        // Check endDate
+        if(block.timestamp > _project.endDate){
+            _project.exists = false;
+            projects[_id] = _project;
+            this.dispatchFunds(_project.id);
+            emit ProjectEnded(_project.id, _project.name, _project.balance);
+        }
+        require(block.timestamp < _project.endDate, "Project is still active.");
+        // Close the project
+        _project.exists = false;
+        projects[_id] = _project;
+        // Trigger the Event
+        emit ProjectEnded(_project.id, _project.name, _project.balance);
+    }
+
+    function balanceOfContract() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function dispatchFunds(uint _id) public payable {
+        Project memory _project = projects[_id];
+        payable( _project.owner).transfer(_project.balance);
+    }
 }
--- test/Crowdfunding.test.js
+++ test/Crowdfunding.test.js
@@ -33,7 +33,8 @@ contract("Crowdfunding", ([deployer, secondAcc, thirdAcc]) => { // revised style
       result = await crowdfunding.createProject("Test Name", "Test Desc", "1622420104", web3.utils.toWei("1", "Ether"), {from : deployer} );
       projectCount = await crowdfunding.projectCount();
     });
-    it("creates products", async ()=>{
+
+    it("creates projects", async ()=>{
       // SUCCESS TESTS
       assert.equal(projectCount, 1);
       const event = result.logs[0].args; // pull from result logs
@@ -53,6 +54,47 @@ contract("Crowdfunding", ([deployer, secondAcc, thirdAcc]) => { // revised style
       //Failure : Project should have endDate
       await crowdfunding.createProject("Test Name", "Test Desc", "", web3.utils.toWei("1", "Ether"), {from : deployer} ).should.be.rejected;
     });
+
+    it("lists projects", async ()=>{
+      // SUCCESS TESTS
+      const project = await crowdfunding.projects(projectCount);
+      assert.equal(project.id.toNumber(), projectCount.toNumber(), 'id is correct');
+      assert.equal(project.name, 'Test Name', "Name is correct");
+      assert.equal(project.desc, 'Test Desc', "Description is correct");
+      assert.equal(project.target, web3.utils.toWei("1", "Ether"), "Target is correct");
+      assert.equal(project.endDate, '1622420104', "Project Close Date is correct");
+      assert.equal(project.exists, true, "Project Status is true");
+      assert.equal(project.balance, 0, "Project Balance is set to 0");
+      //FAILURE TESTS
+    });
+
+    it("fund project", async ()=>{
+      // SUCCESS TESTS
+      //Another User Funded the project
+      const result = await crowdfunding.fundProject(projectCount, {from : secondAcc, value: web3.utils.toWei("1", "Ether") });
+      const event = result.logs[0].args; // pull from result logs
+      assert.equal(event.id.toNumber(), projectCount.toNumber(), 'id is correct');
+      assert.equal(event.name, 'Test Name', "Name is correct");
+      assert.equal(event.target, web3.utils.toWei("1", "Ether"), "Target is correct");
+      assert.equal(event.endDate, '1622420104', "Project Close Date is correct");
+      assert.equal(event.exists, true, "Project Status is true");
+      assert.equal(event.balance, web3.utils.toWei("1", "Ether"), "Project Balance is set to 1");
+      assert.equal(event.owner, secondAcc, 'Contributor is not Owner');
+      //FAILURE TESTS
+    });
+
+    it("close project", async ()=>{
+      // SUCCESS TESTS
+      const result = await crowdfunding.closeProject(projectCount, {from : deployer });
+      const event = result.logs[0].args; // pull from result logs
+      assert.equal(event.id.toNumber(), projectCount.toNumber(), 'id is correct');
+      assert.equal(event.name, 'Test Name', "Name is correct");
+      //FAILURE TESTS
+    });
+
+
+
+
   });
 
 });
