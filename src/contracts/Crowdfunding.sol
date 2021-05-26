afd00b2ea6747b136dc10bf81335b9868862a2dc
rakimsth <raktim@rumsan.com>
2021-05-26 12:22:10 +0545

    project contract for creating new projects with testcases

--- src/contracts/Crowdfunding.sol
+++ src/contracts/Crowdfunding.sol
@@ -9,21 +9,42 @@ contract Crowdfunding {
     struct Project {
         uint id;
         string name;
+        string desc;
         address owner;
-        uint endTime;
+        uint256 endDate;
         bool exists;
         uint balance;
+        uint target;
     }
+
+    event ProjectCreated(
+        uint id,
+        string name,
+        string desc,
+        address owner,
+        uint256 endDate,
+        bool exists,
+        uint balance,
+        uint target
+    );
 
     constructor() {
         name = "Marketplace for crowd funding";
     }
 
-    function createProject() public {
-        // Make sure parameters are correct
+    function createProject(string memory _name, string memory _desc, uint256 _endDate, uint _target) public {
+        // Make sure parameters are correct and supplied
+        // Require a valid name
+        require(bytes(name).length > 0);
+        // Require a valid target
+        require(_target > 0);
+        // Require a valid endDate
+        require(_endDate > 0);
         //Increment Project Count
         projectCount ++;
         // Create the project
+        projects[projectCount] = Project(projectCount, _name, _desc, msg.sender, _endDate, true, 0, _target);
         // trigger an event
+        emit ProjectCreated(projectCount, _name, _desc, msg.sender, _endDate, true, 0, _target);
     }
 }
--- test/Crowdfunding.test.js
+++ test/Crowdfunding.test.js
@@ -2,13 +2,14 @@
 /* eslint-disable no-undef */
 const Crowdfunding = artifacts.require("Crowdfunding");
 
-contract("Crowdfunding", (accounts) => {
+// contract("Crowdfunding", (accounts) => { //old style
+contract("Crowdfunding", ([deployer, secondAcc, thirdAcc]) => { // revised style
   let crowdfunding;
 
   before(async () => {
     crowdfunding = await Crowdfunding.deployed();
   });
-
+  // Test Deployment
   describe("deployment", async () => {
     it("deploys successfully", async () => {
       const address = await crowdfunding.address;
@@ -23,16 +24,30 @@ contract("Crowdfunding", (accounts) => {
       assert.equal(name, "Marketplace for crowd funding");
     });
   });
-
+  // Test Project Creation
   describe("projects", async () => {
     let result, projectCount;
     before(async () => {
-      result = await crowdfunding.createProject();
+      result = await crowdfunding.createProject("Test Name", "Test Desc", "1622420104", web3.utils.toWei("1", "Ether"), {from : deployer} );
       projectCount = await crowdfunding.projectCount();
     });
     it("creates products", async ()=>{
       // SUCCESS
-      assert.equal(projectCount, 1); 
+      assert.equal(projectCount, 1);
+      const event = result.logs[0].args; // pull from result logs
+      assert.equal(event.id.toNumber(), projectCount.toNumber(), 'id is correct');
+      assert.equal(event.name, 'Test Name', "Name is correct");
+      assert.equal(event.desc, 'Test Desc', "Description is correct");
+      assert.equal(event.target, web3.utils.toWei("1", "Ether"), "Target is correct");
+      assert.equal(event.endDate, '1622420104', "Project Close Date is correct");
+      assert.equal(event.exists, true, "Project Status is true");
+      assert.equal(event.balance, 0, "Project Balance is set to 0");
+      // assert.equal(event.owner, accounts[0], 'Owner is correct'); // old style
+      assert.equal(event.owner, deployer, 'Owner is correct'); // revised style
+      
+      // Failure
+
+
     });
   });
 
