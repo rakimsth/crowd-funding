a179acc12bbebab3127af4270ff5b6d555a6985e
rakimsth <raktim@rumsan.com>
2021-05-26 23:55:18 +0545

    basic CSS layout setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-1 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="https://rumsan.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rumsan Academy
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>;
 }
 
 export default App;
